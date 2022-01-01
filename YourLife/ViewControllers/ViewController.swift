6f510d6edcc4bc9620287098fa03d3444f53ebde
Ovechkina Diana <ovechkinadiana@yandex.ru>
2022-01-01 17:41:03 +0300

    check commits

--- YourLife/ViewControllers/ViewController.swift
+++ YourLife/ViewControllers/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var dateTextField: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dateTextField.inputView = datePicker
+        datePicker.datePickerMode = .date
+        
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(didTapDone)
+        )
+        
+        let flexBarButton = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        
+        toolbar.items = [flexBarButton, doneButton]
+
+        
+        dateTextField.inputAccessoryView = toolbar
+        
+    }
+    
+    @objc private func didTapDone() {
+        getDateFromPicker()
+        view.endEditing(true)
     }
 
+    func getDateFromPicker() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateTextField.text = dateFormatter.string(from: datePicker.date)
+    }
 
 }
 
