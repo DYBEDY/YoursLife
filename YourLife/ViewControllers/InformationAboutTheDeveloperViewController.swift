94b3a6601339e77f10ca4c5f62dd27f7997ca2dd
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-02 21:15:20 +0300

    Developer information screen

---
+++ YourLife/ViewControllers/InformationAboutTheDeveloperViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InformationAboutTheDeveloperViewController.swift
+//  YourLife
+//
+//  Created by Елисей Мищенко on 02.01.2022.
+//
+
+import UIKit
+
+class InformationAboutTheDeveloperViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
