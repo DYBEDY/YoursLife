a9b6aec888f13fc7609d5aeb78c1e9b3621d4c6e
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-02 20:16:10 +0300

    Working with the ApplicationDescriptionVC screen, adding a label and working with logic.

--- YourLife/ViewControllers/ApplicationDescriptionViewController.swift
+++ YourLife/ViewControllers/ApplicationDescriptionViewController.swift
@@ -9,10 +9,11 @@ import UIKit
 
 class ApplicationDescriptionViewController: UIViewController {
 
+    @IBOutlet weak var informationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        informationLabel.text = "In this application you can find out how long you have lived and how much you still want to live"
     }
 
 }
