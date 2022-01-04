6d9c3571ca3f25c57d99f587c70319286fd4b925
DYBEDY <rd.iosdeveloper@gmail.com>
2022-01-04 03:22:34 +0300

    Added four screens Added all alerts

--- YourLife/ViewControllers/LoginViewController.swift
+++ YourLife/ViewControllers/LoginViewController.swift
@@ -13,8 +13,8 @@ class LoginViewController: UIViewController {
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var surnameTextField: UITextField!
     
-    @IBOutlet var birthdayDatePicker: UIDatePicker!
-    @IBOutlet var finishDatePicker: UIDatePicker!
+    @IBOutlet var birthdayDate: UITextField!
+    @IBOutlet var finishDate: UITextField!
     
     @IBOutlet var progressView: UIProgressView!
     
@@ -24,13 +24,36 @@ class LoginViewController: UIViewController {
     private var steps = LoginSteps.takeAStep()
     private var currentStepIndex = 0
     
+    let birthdayDatePicker = UIDatePicker()
+    let finishDatePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        
+        nameTextField.delegate = self
+        surnameTextField.delegate = self
+        
+        nameTextField.returnKeyType = .next
+        
+        birthdayDate.delegate = self
+        finishDate.delegate = self
+        
+        addDoneButtonTo(surnameTextField, birthdayDate, finishDate)
        
     }
     @IBAction func nextButtonPressed() {
-        getSecondScreen()
+        guard let nameText = nameTextField.text else { return }
+        guard let surnameText = surnameTextField.text else { return }
+        if nameText.isEmpty || surnameText.isEmpty {
+            showAlert(tittle: "Something went wrong", message: "Input correct information")
+        } else {
+            getSecondScreen()
+        }
+         
+
+
+        
         
     }
     
@@ -73,13 +96,12 @@ extension LoginViewController {
         nameTextField.isHidden = false
         surnameTextField.isHidden = false
        
-        birthdayDatePicker.isHidden = true
-        finishDatePicker.isHidden = true
+        birthdayDate.isHidden = true
+        finishDate.isHidden = true
         
         startButton.isHidden = true
         
         title = "Знакомство"
-
     }
     
     private func applySettingsForSecondScreen() {
@@ -88,8 +110,9 @@ extension LoginViewController {
         nameTextField.isHidden = true
         surnameTextField.isHidden = true
         
-        birthdayDatePicker.isHidden =  false
-        finishDatePicker.isHidden = true
+        birthdayDate.isHidden = false
+        finishDate.isHidden = true
+        birthdayPicker()
         
         startButton.isHidden = true
         
@@ -103,8 +126,9 @@ extension LoginViewController {
         nameTextField.isHidden = true
         surnameTextField.isHidden = true
         
-        birthdayDatePicker.isHidden = true
-        finishDatePicker.isHidden = false
+        birthdayDate.isHidden = true
+        finishDate.isHidden = false
+        finishPicker()
         
         startButton.isHidden = false
         nextButton.isHidden = true
@@ -115,22 +139,170 @@ extension LoginViewController {
     
     
     private func getSecondScreen() {
-        currentStepIndex += 1
         
-        if currentStepIndex < steps.count {
-            setupUI()
-            return
+        if currentStepIndex == 0 {
+            guard let nameText = nameTextField.text else { return }
+            guard let surnameText = surnameTextField.text else { return }
+            if nameText.isEmpty || surnameText.isEmpty {
+                showAlert(tittle: "Something went wrong", message: "Input correct information")
+            } else {
+                currentStepIndex += 1
+            }
+            
+        } else if currentStepIndex == 1 {
+            guard let birthdayText = birthdayDate.text else { return }
+            if birthdayText.isEmpty {
+                showAlert(tittle: "Something went wrong", message: "Input correct information")
+            } else {
+                currentStepIndex += 1
+            }
         } else {
+            guard let finishText = finishDate.text else { return }
+            if finishText.isEmpty {
+                showAlert(tittle: "Something went wrong", message: "Input correct information")
+            } else {}
+                
+            }
+            
+            
+            if currentStepIndex < steps.count {
+                setupUI()
+                return
+            } else {
+                
+            }
             
         }
+    }
+
+
+
+// MARK: - datePicker methods
+extension LoginViewController: UITextFieldDelegate {
+    private func birthdayPicker() {
+        birthdayDate.inputView = birthdayDatePicker
+        birthdayDatePicker.datePickerMode = .date
+        birthdayDatePicker.preferredDatePickerStyle = .wheels
         
+        let minimumYear = Calendar.current.date(byAdding: .year, value: -80, to: Date())
+        let maximumYaer = Calendar.current.date(byAdding: .year, value: -5, to: Date())
+        
+        birthdayDatePicker.minimumDate = minimumYear
+        birthdayDatePicker.maximumDate = maximumYaer
+        
+        birthdayDatePicker.addTarget(self, action: #selector(dateChangedBirth), for: .valueChanged)
     }
     
-    private func birthdayDatePickerSettings() {
+    @objc func dateChangedBirth() {
+        getBirthDateFromPicker()
+        }
+    
+    private func getBirthDateFromPicker() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        birthdayDate.text = formatter.string(from: birthdayDatePicker.date)
+    }
+    
+    private func finishPicker() {
+        finishDate.inputView = finishDatePicker
+        finishDatePicker.datePickerMode = .date
+        finishDatePicker.preferredDatePickerStyle = .wheels
+        
+        let minimumYear = Calendar.current.date(byAdding: .year, value: 5, to: Date())
+        let maximumYaer = Calendar.current.date(byAdding: .year, value: 120, to: Date())
+        
+        finishDatePicker.minimumDate = minimumYear
+        finishDatePicker.maximumDate = maximumYaer
+        
+        finishDatePicker.addTarget(self, action: #selector(dateChangedFinish), for: .valueChanged)
+    }
+
+    @objc func dateChangedFinish() {
+        getFinishDateFromPicker()
+    }
+   
+    private func getFinishDateFromPicker() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        finishDate.text = formatter.string(from: finishDatePicker.date)
+    }
+    
+    private func addDoneButtonTo(_ textFields: UITextField...) {
+           
+           let numberToolbar = UIToolbar()
+           numberToolbar.sizeToFit()
+           textFields.forEach { textField in
+               switch textField {
+               case surnameTextField: textField.inputAccessoryView = numberToolbar
+               case birthdayDate: textField.inputAccessoryView = numberToolbar
+               case finishDate: textField.inputAccessoryView = numberToolbar
+               default: break
+               }
+           }
+           
+           let doneButton = UIBarButtonItem(title:"Done",
+                                            style: .done,
+                                            target: self,
+                                            action: #selector(tapDone))
+           
+           let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                               target: nil,
+                                               action: nil)
+           
+           numberToolbar.items = [flexBarButton, doneButton]
+           
+       }
        
-        
-        
+       @objc private func tapDone() {
+           view.endEditing(true)
+       }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            view.endEditing(true)
+            super.touchesBegan(touches , with:event)
+        }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == nameTextField {
+            surnameTextField.becomeFirstResponder()
+        }
+        return false
     }
     
+    
+    internal func textFieldDidEndEditing(_ textField: UITextField) {
+            guard let newValue = textField.text else { return }
+        if let _ = Float(newValue) {
+            showAlert(tittle: "Something went wrong", message: "Input correct information")
+            nameTextField.text = ""
+            surnameTextField.text = ""
+        }
+        if let _ = Int(newValue) {
+            showAlert(tittle: "Something went wrong", message: "Input correct information")
+            nameTextField.text = ""
+            surnameTextField.text = ""
+        }
+        }
 }
+
+// Mark: - Alerts
+
+extension LoginViewController {
+    private func showAlert(tittle: String, message: String) {
+        let alert = UIAlertController(title: tittle, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default)
+
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
+}
+
+
+    
+    
+    
+    
+    
+    
+    
 
